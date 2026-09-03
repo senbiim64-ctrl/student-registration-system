8a6475299abdd1f518e8d5532bcf9c79cd4409fc
Senbii Mulatu <senbiim64@gmail.com>
2026-09-03 14:10:36 +0300

    Prepare Django for deployment

--- student_registration/settings.py
+++ student_registration/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k_)vcsp$vqksz^lvi)ufk0-^$9l&=u_=s1j^(kwbmp!-k1_=ra
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
